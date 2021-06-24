dfc017824d6c7b55b931eb0e235a4e47ae6d78a3
Bartek Wlodarski <wlodarsb@coventry.ac.uk>
2021-06-24 02:01:30 +0100

    Created second version of clustering algorithm

--- KMeansClustering.py
+++ KMeansClustering.py
@@ -1,11 +1,11 @@
 # %% Imports
 
+from sklearn.cluster import KMeans
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import MinMaxScaler
 import pandas as pd
 import copy
 import numpy as np
-import seaborn as sns
 import matplotlib.pyplot as plt
 
 # %% Classes
@@ -58,7 +58,7 @@ class ModifiedKMeans:
 
     def _euclidean_dist(self, t: np.ndarray, c: np.ndarray) -> float:
         """Calculates the Euclidean Distance between datapoint t and centroid c."""
-        square_dist = sum((t - c) ** 2)
+        square_dist = np.sum((t - c) ** 2)
         return np.sqrt(square_dist)
 
     def _k_means_objective(self, data_points: np.ndarray):
@@ -156,16 +156,223 @@ encodedDf
 
 # %%
 
-pca = PCA(n_components=2)  # type: ignore
+# pca = PCA(n_components=2)  # type: ignore
 
-pcaDf = pca.fit_transform(encodedDf)
-pcaDf
+# pcaDf = pca.fit_transform(encodedDf)
+# pcaDf
+
+# scaler = MinMaxScaler()
+
+# scaledDf = scaler.fit_transform(pcaDf)
+# scaledDf
+
+# plt.scatter(scaledDf[:, 0], scaledDf[:, 1])
+
+# %%
+
+# Reading the filtered ratings table
+
+ratings_table = pd.read_csv('data/filtered_ratings.csv')
+ratings_table
+
+
+# %%
+
+# Loading the genres table
+
+genres_table = pd.read_csv('data/movies.csv')
+genres_table
+
+# %%
+
+# Dropping movie title and encoding the genres into columns
+
+encodings = genres_table['genres'].str.split('|').explode()
+updated_genres = genres_table.drop(columns=['title', 'genres'])
+crosstab = pd.crosstab(encodings.index, encodings)
+
+crosstab
+
+# %%
+
+# Clustering using regular KMeans
+
+
+# kmeans = KMeans(n_clusters=5)
+# kmeans.fit_transform(encoded_movie_genres)
+# kmeans.cluster_centers_
 
 scaler = MinMaxScaler()
 
-scaledDf = scaler.fit_transform(pcaDf)
-scaledDf
+scaled = scaler.fit_transform(crosstab)
 
-plt.scatter(scaledDf[:, 0], scaledDf[:, 1])
+pca = PCA(n_components=2)
+pca_movie_genres = pca.fit_transform(scaled)
+pca_movie_genres
+
+# plt.scatter(x=pca_movie_genres[:, 0], y=pca_movie_genres[:, 1])
+
+# kmeans = KMeans(n_clusters=6)
+# pca_clusters = kmeans.fit_predict(pca_movie_genres)
+
+# pca_clusters
+
+# plt.scatter(x=pca_movie_genres[:, 0], y=pca_movie_genres[:, 1], c=pca_clusters)  # type:ignore
+
+# cluster_centers = kmeans.cluster_centers_
+
+# plt.scatter(x=cluster_centers[:, 0], y=cluster_centers[:, 1],
+#             marker='^', color='k', edgecolors='w')  # type:ignore
 
 # %%
+
+# 2nd attempt at the modified k-means
+
+
+class ModifiedKMeansV2:
+    """Modified k-means clustering algorithm. Takes in a number of clusters."""
+
+    def __init__(self, n_clusters: int):
+        """n_clusters: number of clusters to create."""
+
+        self.n_clusters: int = n_clusters
+        self.centroids = np.empty((n_clusters, 2))
+        self.thresholds = np.empty((n_clusters,))
+
+    def _set_centroids(self, data_points: np.ndarray) -> None:
+        """Picks random data points as cluster centroids and assigns them."""
+        # Get the number of data points
+        row_count = data_points.shape[0]
+        # Pick n random indexes to select rows as starting centroids
+        centroids = np.random.choice(
+            row_count, size=self.n_clusters, replace=False)
+        # Picking the datapoints as centroids
+        centroids = data_points[centroids]
+        # Assigning to the Cluster dataclass
+        self.centroids = centroids
+
+    def _euclidean_dist(self, t: np.ndarray, c: np.ndarray) -> float:
+        """Calculates the Euclidean Distance between datapoint t and centroid c."""
+        return np.sqrt(np.sum((t - c) ** 2))
+
+    def _k_means_objective(self, data_points: np.ndarray):
+        """Uses the initial objective function to set new clusters for the first iteration of the clustering process."""
+        for i, point in enumerate(data_points):
+            smallest_dist = np.Infinity
+            closest = -1
+            for j, centroid in enumerate(self.centroids):
+                distance = self._euclidean_dist(point, centroid)
+                if distance < smallest_dist:
+                    smallest_dist = distance
+                    closest = j
+                # Adding the distance between cluster centroid and point to list
+                self.distances[j, i] = distance
+            # Assigning the point to the closest centroid cluster
+            self.points[closest, i] = True
+
+    def _calc_threshold(self, distances: np.ndarray):
+        return np.mean(distances)
+
+    def _modified_k_means_objective(self, data_points: np.ndarray):
+        """The modified objective function which uses thresholds to assign points to the clusters."""
+        for i, point in enumerate(data_points):
+            assigned = False
+            smallest_dist = np.Infinity
+            closest = -1
+            for j, centroid in enumerate(self.centroids):
+                distance = self._euclidean_dist(point, centroid)
+                self.distances[j, i] = distance
+                if distance < smallest_dist:
+                    smallest_dist = distance
+                    closest = j
+                if distance <= self.thresholds[j]:
+                    self.points[j, i] = True
+                    assigned = True
+            if not assigned:
+                self.points[closest, i] = True
+
+    def _new_centroid(self, points: np.ndarray):
+        """Calculates the new cluster centroid based on the points in the cluster."""
+        return np.mean(points, axis=0)
+
+    def cluster_data(self, iters: int, data_points: np.ndarray):
+        """Clusters the dataset using the modified k-means clustering algorithm."""
+        # Creating a 2d array which identifies which cluster each point belongs to
+        # By default, each point is not assigned to any cluster, so the value is False
+        self.points = np.full((self.n_clusters, data_points.shape[0]), False)
+        # Setting the distances between each point and cluster centroid
+        # By default the values are all set to infinity as the distance
+        # at this point has not been measuered yet
+        self.distances = np.full((self.n_clusters, data_points.shape[0]), np.Infinity)
+        self._set_centroids(data_points)
+        self._k_means_objective(data_points)
+        for iteration in range(iters):
+            old_points = np.copy(self.points)
+            # for j, cluster in enumerate(self.clusters):
+            for j in range(self.n_clusters):
+                # Assigning a new threshold to each cluster
+                # By getting the distances between each point in each cluster
+                self.thresholds[j] = self._calc_threshold(self.distances[j])
+                # Getting the data points in the current cluster
+                cluster_points = data_points[self.points[j]]
+                # Calculating new centroid based on the average position
+                # of the data points in the given cluster
+                self.centroids[j] = self._new_centroid(cluster_points)
+            # Resetting the values of distances and points assigned to cluster
+            self.distances = np.full((self.n_clusters, data_points.shape[0]), np.Infinity)
+            self.points = np.full((self.n_clusters, data_points.shape[0]), False)
+            self._modified_k_means_objective(data_points)
+            if np.array_equal(old_points, self.points):
+                print(f'\nFinished in {iteration+1} iterations')
+                break
+            print(f'Finished iteration {iteration+1}')
+        else:
+            # If the iteration limit is reached
+            print('\nIteration limit reached')
+        return self.points
+
+# %%
+
+# Using the custom clusteirng algorithm
+
+
+n_clusters = 6
+
+modified_kmeans = ModifiedKMeansV2(n_clusters=n_clusters)
+
+clusters = modified_kmeans.cluster_data(iters=100, data_points=pca_movie_genres)
+clusters
+
+# %%
+
+# Getting the points in each cluster
+cluster_points = [pca_movie_genres[cluster] for cluster in clusters]
+centroids = modified_kmeans.centroids
+
+# %%
+
+for cluster in cluster_points:
+    plt.scatter(cluster[:, 0], cluster[:, 1])
+
+plt.scatter(x=centroids[:, 0], y=centroids[:, 1], marker='^',  # type:ignore
+            color='k', edgecolors='w')
+
+plt.xlabel('Principal component 1')
+plt.ylabel('Principal component 2')
+
+# %%
+
+fig, axs = plt.subplots(n_clusters, figsize=(10, 6 * n_clusters))
+for i, cluster in enumerate(cluster_points):
+    axs[i].scatter(cluster[:, 0], cluster[:, 1])
+    axs[i].scatter(centroids[i, 0], centroids[i, 1], marker='^', color='k', edgecolors='w')
+    axs[i].title.set_text(f'Cluster {i+1}')
+
+plt.savefig('images/separate.png', transparent=False)
+
+# %%
+
+# Saving data to csv
+
+df = pd.DataFrame(clusters, columns=genres_table['movieId'])  # type:ignore
+df.to_csv('data/clustered_movies.csv')
